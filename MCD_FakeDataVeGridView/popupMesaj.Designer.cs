81c34a8895e06d2f6b147e1ddce4d7d91b6b6194
emre gemici <gmcc.emree@gmail.com>
2021-12-10 19:41:44 +0300

    code changed

---
+++ MCD_FakeDataVeGridView/popupMesaj.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace MCD_FakeDataVeGridView
+{
+    partial class popupMesaj
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtİsim = new System.Windows.Forms.TextBox();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.txtTamAd = new System.Windows.Forms.TextBox();
+            this.txtIlIlce = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtEmailAdres = new System.Windows.Forms.TextBox();
+            this.txtTelefonNo = new System.Windows.Forms.TextBox();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(172, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 21);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Adres:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(172, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 21);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Email Adres:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(172, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "İl / İlçe :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tam isim:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Soyisim:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "İsim:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(172, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 21);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Telefon Numarası:";
+            // 
+            // txtİsim
+            // 
+            this.txtİsim.Location = new System.Drawing.Point(344, 76);
+            this.txtİsim.Name = "txtİsim";
+            this.txtİsim.Size = new System.Drawing.Size(248, 28);
+            this.txtİsim.TabIndex = 7;
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(344, 115);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(248, 28);
+            this.txtSoyisim.TabIndex = 7;
+            // 
+            // txtTamAd
+            // 
+            this.txtTamAd.Location = new System.Drawing.Point(344, 156);
+            this.txtTamAd.Name = "txtTamAd";
+            this.txtTamAd.Size = new System.Drawing.Size(248, 28);
+            this.txtTamAd.TabIndex = 7;
+            // 
+            // txtIlIlce
+            // 
+            this.txtIlIlce.Location = new System.Drawing.Point(344, 197);
+            this.txtIlIlce.Name = "txtIlIlce";
+            this.txtIlIlce.Size = new System.Drawing.Size(248, 28);
+            this.txtIlIlce.TabIndex = 7;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(344, 237);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(248, 28);
+            this.txtAdres.TabIndex = 7;
+            // 
+            // txtEmailAdres
+            // 
+            this.txtEmailAdres.Location = new System.Drawing.Point(344, 276);
+            this.txtEmailAdres.Name = "txtEmailAdres";
+            this.txtEmailAdres.Size = new System.Drawing.Size(248, 28);
+            this.txtEmailAdres.TabIndex = 7;
+            // 
+            // txtTelefonNo
+            // 
+            this.txtTelefonNo.Location = new System.Drawing.Point(344, 311);
+            this.txtTelefonNo.Name = "txtTelefonNo";
+            this.txtTelefonNo.Size = new System.Drawing.Size(248, 28);
+            this.txtTelefonNo.TabIndex = 7;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Location = new System.Drawing.Point(384, 371);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(178, 60);
+            this.btnKapat.TabIndex = 8;
+            this.btnKapat.Text = "KAPAT";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // popupMesaj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 586);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.txtTelefonNo);
+            this.Controls.Add(this.txtEmailAdres);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtIlIlce);
+            this.Controls.Add(this.txtTamAd);
+            this.Controls.Add(this.txtSoyisim);
+            this.Controls.Add(this.txtİsim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "popupMesaj";
+            this.Text = "popupMesaj";
+            this.Load += new System.EventHandler(this.popupMesaj_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtİsim;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.TextBox txtTamAd;
+        private System.Windows.Forms.TextBox txtIlIlce;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtEmailAdres;
+        private System.Windows.Forms.TextBox txtTelefonNo;
+        private System.Windows.Forms.Button btnKapat;
+    }
+}
---
+++ MCD_FakeDataVeGridView/popupMesaj.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_FakeDataVeGridView
+{
+    public partial class popupMesaj : Form
+    {
+        Musteri musteriData;
+        public popupMesaj(Musteri data)
+        {
+            InitializeComponent();
+            musteriData = data;
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void popupMesaj_Load(object sender, EventArgs e)
+        {
+            txtİsim.Text = musteriData.isim;
+            txtSoyisim.Text = musteriData.soyisim;
+            txtTamAd.Text = musteriData.tamAdi;
+            txtEmailAdres.Text = musteriData.emailAdres;
+            txtTelefonNo.Text = musteriData.telefonNumara;
+            txtIlIlce.Text = $"{musteriData.il} / { musteriData.ilce}";
+            txtAdres.Text = musteriData.adres;
+        }
+    }
+}
