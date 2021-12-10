cdff433c9c5981d9cc1be8a97ea9321ae3462b31
emre gemici <gmcc.emree@gmail.com>
2021-12-10 19:25:57 +0300

    code changed

--- MCD_FakeDataVeGridView/Form1.Designer.cs
+++ MCD_FakeDataVeGridView/Form1.Designer.cs
@@ -29,21 +29,52 @@ namespace MCD_FakeDataVeGridView
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgwMusteriListe = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMusteriListe)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgwMusteriListe);
+            this.groupBox1.Location = new System.Drawing.Point(33, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(781, 578);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Müşteri Listesi";
+            // 
+            // dgwMusteriListe
+            // 
+            this.dgwMusteriListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwMusteriListe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgwMusteriListe.Location = new System.Drawing.Point(3, 18);
+            this.dgwMusteriListe.Name = "dgwMusteriListe";
+            this.dgwMusteriListe.RowHeadersWidth = 51;
+            this.dgwMusteriListe.RowTemplate.Height = 24;
+            this.dgwMusteriListe.Size = new System.Drawing.Size(775, 557);
+            this.dgwMusteriListe.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 649);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMusteriListe)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgwMusteriListe;
     }
 }
 
--- MCD_FakeDataVeGridView/Form1.cs
+++ MCD_FakeDataVeGridView/Form1.cs
@@ -19,6 +19,37 @@ namespace MCD_FakeDataVeGridView
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //string isim = FakeData.NameData.GetFirstName();
+            //string soyisim = FakeData.NameData.GetSurname();
+            //MessageBox.Show(isim + " " + soyisim, "Fake Data İnceleme", MessageBoxButtons.OK);
+
+            Database db = new Database();
+            List<Musteri> musteriListe = db.musteriListele();
+
+            // 1.Data bize lazım ama ekran üzerinde göstermek istemiyoruz bu gibi durumlarda columns koleksiyonu içinde ilgili kolonun id değeri veya tip prop adı verilerek Visible prop false edilmesi yeterlidir.
+            //dgwMusteriListe.DataSource = musteriListe;
+            //dgwMusteriListe.Columns[0].Visible = false;
+            //dgwMusteriListe.Columns["id"].Visible = true;
+
+            //Data Grid View içerisinde bulunan kolonların isimlerini değiştirmek...
+
+            //dgwMusteriListe.Columns[0].HeaderText = "Müşteri ID";
+            //dgwMusteriListe.Columns[1].HeaderText = "Müşteri İsim";
+            //dgwMusteriListe.Columns[2].HeaderText = "Müşteri Soyisim";
+            //dgwMusteriListe.Columns[2].Width = 500;
+            //dgwMusteriListe.Columns[3].HeaderText = "Müşteri Tam Adı ";
+
+            //2.Data bize lazım değil hiç bir şekilde ekranda görünmesini veya kullanmak istemiyoruz.
+
+            var dgwListe = from I in musteriListe
+                           select new
+                           {
+                               Isim = I.isim,
+                               Soyisim = I.soyisim,
+                               TamAdi = I.tamAdi
+                           };
+
+            dgwMusteriListe.DataSource = dgwListe.ToList();
 
         }
     }
